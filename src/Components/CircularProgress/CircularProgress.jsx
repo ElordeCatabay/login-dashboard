840f16c7277ba4dbe216442c4e967594817ef62d
ElordeCatabay <elgu.catabay.up@phinmaed.com>
2023-09-30 15:22:35 +0800

    first commit

---
+++ src/Components/CircularProgress/CircularProgress.jsx
@@ -0,0 +1,27 @@
+import './CircularProgress.css'
+
+// eslint-disable-next-line react/prop-types
+const CircularProgress = ({ value, offset, color }) => {
+  return (
+    <div className='circular-progress'>
+      <div className='details'>
+        <h1>{value}</h1>
+        <p>%</p>
+      </div>
+
+      <svg>
+        <circle cx='55' cy='55' r='50' className='svg-circle-gray' />
+
+        <circle
+          cx='55'
+          cy='55'
+          r='50'
+          className='svg-circle'
+          style={{ '--clr': color, '--p': offset }}
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default CircularProgress
